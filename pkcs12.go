ae8601303f3db99318041ad32c20738fdfe893f8
Luca Morgado <luca.morgado.pro@gmail.com>
2025-12-16 10:01:42 +0100

    fix: chain missing first certificate (#4)
    Co-authored-by: tot0p <thomas.lemaitre.78420@gmail.com>

--- pkcs12.go
+++ pkcs12.go
@@ -30,8 +30,9 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
-	"golang.org/x/crypto/ed25519"
 	"io"
+
+	"golang.org/x/crypto/ed25519"
 )
 
 // DefaultPassword is the string "changeit", a commonly-used password for
@@ -474,7 +475,7 @@ func DecodeChain(pfxData []byte, password string) (privateKey interface{}, certi
 	}
 
 	// certificate value should be the one where the private key exists
-	for _, certInP12 := range caCerts {
+	for index, certInP12 := range caCerts {
 		match := false
 		switch casted := certInP12.PublicKey.(type) {
 		case *rsa.PublicKey:
@@ -487,7 +488,17 @@ func DecodeChain(pfxData []byte, password string) (privateKey interface{}, certi
 			// Do nothing, will not match
 		}
 		if match {
+			previousCert := certificate // store previous certificate
 			certificate = certInP12
+			// remove certInP12 from caCerts
+			if index < len(caCerts)-1 {
+				caCerts = append(caCerts[:index], caCerts[index+1:]...)
+			} else {
+				caCerts = caCerts[:index]
+			}
+			// put previous certificate into caCerts at the beginning
+			caCerts = append([]*x509.Certificate{previousCert}, caCerts...)
+			break
 		}
 	}
 
